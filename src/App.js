d5265b11429d3f6067941a700150e65ff51eede0
vivekpandeyait <vivek496pandey@gmail.com>
2021-02-11 19:50:21 +0530

    VIVEK PANDEY

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Header from './Component/Header.js';
+import Home from './Component/Home.js';
+import Button from 'react-bootstrap/Button';
+import Contact from './Component/Contact';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+      <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">VIVEK PANDEY</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/about">About</Nav.Link>
+      <Nav.Link href="/contact">Contact</Nav.Link>
+     
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+
+      
+        <Switch>
+          <Router path="/contact">
+            <Contact />
+          </Router>
+          <Route path="/about">
+          <Header />
+          </Route>
+          
+          <Route path="/">
+          <Home />
+           
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+     
+    
     </div>
   );
 }
