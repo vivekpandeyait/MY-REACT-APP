f4470c25257381ed4acbef620912728f8ce4877e
vivekpandeyait <vivek496pandey@gmail.com>
2021-02-11 20:11:58 +0530

    Updates

---
+++ precache-manifest.8d718d525d61673baec68e2ad8abde4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d8c3fbadce8b8019841b7db7558501",
+    "url": "/MY-REACT-APP/index.html"
+  },
+  {
+    "revision": "d96eea7740b31635359a",
+    "url": "/MY-REACT-APP/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8aa58dd7596e6c4cb216",
+    "url": "/MY-REACT-APP/static/css/main.ee52de67.chunk.css"
+  },
+  {
+    "revision": "d96eea7740b31635359a",
+    "url": "/MY-REACT-APP/static/js/2.d84c10f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MY-REACT-APP/static/js/2.d84c10f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa58dd7596e6c4cb216",
+    "url": "/MY-REACT-APP/static/js/main.794c7a38.chunk.js"
+  },
+  {
+    "revision": "dcd183fb10fd9221fa5e",
+    "url": "/MY-REACT-APP/static/js/runtime-main.c77c2494.js"
+  }
+]);
